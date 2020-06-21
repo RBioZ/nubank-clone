8d703c7e8687714640bd40949bc4f316596d7a83
RBioZ <jonatha_rihan@hotmail.com>
2020-06-21 05:51:42 -0300

    estrutura básica

---
+++ src/pages/Main/index.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import {StatusBar} from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import {Container, Content, Card, CardHeader, CardContent, CardFooter, Title, Description, Annotation} from './styles';
+import Header from '../../components/Header';
+import Tabs from '../../components/Tabs';
+import Menu from '../../components/Menu';
+export default function Main(){
+  return(
+    <>
+      <StatusBar barStyle="light-content" backgroundColor="#8B10AE" />
+      <Container>
+				<Header />
+
+				<Content>
+					<Menu />
+
+
+					<Card>
+						<CardHeader>
+							<Icon name="attach-money" size={28} color="#666" />
+							<Icon name="visibility-off" size={28} color="#666" />
+						</CardHeader>
+						<CardContent>
+							<Title>Saldo disponivel</Title>
+							<Description>R$ 24.575,95</Description>
+						</CardContent>
+						<CardFooter>
+							<Annotation>
+								Transferência de R$ 20,00 recebida de José hoje às 00:00h
+							</Annotation>
+						</CardFooter>
+					</Card>
+				</Content>
+
+
+				<Tabs />
+			</Container>
+    </>
+  );
+}
